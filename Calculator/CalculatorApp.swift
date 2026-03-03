6a289ca0fa7ec46119b0fc7acb7b23e8f315cce8
Justin Bushfield <justinbushfield@gmail.com>
2026-03-03 09:44:15 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Justin Bushfield on 2026-03-03.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
